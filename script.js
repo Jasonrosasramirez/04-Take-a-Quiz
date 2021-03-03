7ef7f279d17e7f44571fe70e3924762013956ec2
Jason Rosas Ramirez <jrosasrjf@gmail.com>
2021-03-03 00:17:55 -0600

    push 3. Producing a greater foundation. Setting up categories and classes

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 var quizTimeRemaining = 180; //The total time for the quiz left in seconds.
 var quizTimeDeducation = 10; // This is the amount of time lost per wrong answer in seconds. 
+
+var scoreTotal; 
+var scoreObtained; 
+var scorePercentage; 
+var scoreLetterGrade; 
