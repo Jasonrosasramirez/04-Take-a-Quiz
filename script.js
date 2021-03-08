c261f3ebd1157ea3180b291320e36b8d63003095
Jason Rosas Ramirez <jrosasrjf@gmail.com>
2021-03-08 00:54:30 -0600

    timer now displays. I was missing innerHTML. The event listener auto triggers even though i don't click for some reason

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
-var quizTimeRemaining = 180; //The total time for the quiz left in seconds.
+
 var quizTimeDeducation = 10; // This is the amount of time lost per wrong answer in seconds. 
 
 
-// relates HTML elements to Javsascript Variables 
-var countDownVariable = document.querySelector("#countdownID");
-var beginTheQuizButton = document.querySelector(".StartSubmitButton");
-countDownVariable = 10;
+var countDownVariable = document.querySelector('#countdownID'); //
+var beginTheQuizButton = document.querySelector('.StartSubmitButton');
+var countDownTimerID = 10; 
+
+beginTheQuizButton.addEventListener('click', IamTheQuiz());
+
+function CountDownFunction() {
+    setInterval(function(){ // iterates once every 1000 ms (or 1s) 
+        countDownVariable.innerHTML = countDownTimerID; // innerHTML specifies rge HTML content of a variable. This links the variable to the HTML
+        countDownTimerID -= 1; 
+        if (countDownTimerID <= 0) {
+            alert("Hey! Time is up. Show the results screen");
+            countDownTimerID = 10;
+        }
+    }, 1000)
+    logmee();
+}
 
 
-beginTheQuizButton.addEventListener("click", StartQuizFunction());
 
-
-    
-
-function StartQuizFunction() { 
-    console.log("Hey! I have been clicked" + " \nI am the countdown: " + countDownVariable);
-    
-    setInterval(function(){
-        countDownVariable -= 1;}, 1000) // decreases the quiz timer as the quiz goes on.
-    
-
+function IamTheQuiz(){
+    CountDownFunction();
 
 }
+
+
+
+function logmee() {
+    console.log("The submit button was clicked");
+
+}
+
+
 
 
 
