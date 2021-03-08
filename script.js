23a8d23ed462c54b04f6f008a875b6c454851164
Jason Rosas Ramirez <jrosasrjf@gmail.com>
2021-03-07 21:51:52 -0600

    seperated the first submit button from the other submit buttons using ID. Planning to make first submit button begin the countdown

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 var quizTimeRemaining = 180; //The total time for the quiz left in seconds.
 var quizTimeDeducation = 10; // This is the amount of time lost per wrong answer in seconds. 
 
-var scoreTotal; 
-var scoreObtained; 
-var scorePercentage; 
-var scoreLetterGrade; 
+
+
+// var buttonName = document.querySelector(".Submit")
+// buttonName.addEventListener("click", logmee()); //function 
+
+
+
+
+function logmee() {
+    console.log("The submit button was clicked");
+
+}
+
+function YourGrade(){
+    var scoreTotal = 100; 
+    var scoreObtained; // will need some query selection 
+    var scorePercentage = scoreObtained / scoreTotal; 
+    var scoreLetterGrade = ""; //This will probably be an if statement 
+
+    console.log("Score total is " + scoreTotal + "score obtained " +  scoreObtained + "score Percentage" + scorePercentage + " Score Letter Grade: " + scoreLetterGrade);
+
+} // Executed at the end of the quiz
+
